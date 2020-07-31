093b4c6b953310f9c87e9cbc4ee268925fb4aed0
Elliott Pope <34248576+elliottpope@users.noreply.github.com>
2020-07-31 18:16:41 -0400

    Spring 2.2.5 upgrade (#1)
    * Test commit with Spring Boot 2.2.5.RELEASE version
    
    * Test commit with Spring Boot 2.2.5.RELEASE version
    
    * update gitignore
    
    * Delete IML file
    
    * FIxing gitignore issue

--- src/test/java/elliott/pope/authmechanisms/controller/TestControllerTest.java
+++ src/test/java/elliott/pope/authmechanisms/controller/TestControllerTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = TestMultipleAuthRunner.class)
+@SpringBootTest(classes = {TestMultipleAuthRunner.class})
 @AutoConfigureMockMvc
 public class TestControllerTest {
 
